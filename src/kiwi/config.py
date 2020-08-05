97721bc57996fd630bcf8903e49e515d095d2548
Jörn-Michael Miehe <Joern-Michael.Miehe@Lenaisten.de>
2020-08-05 02:08:55 +0200

    Crude init function

--- src/kiwi/config.py
+++ src/kiwi/config.py
@@ -2,50 +2,57 @@ import os
 import yaml
 
 
-
 class Config:
-    __content = None
+    KIWI_ROOT = os.getenv('KIWI_ROOT', '.')
+    __ymlContent = None
 
-    def __init__(self, filename):
+    def __init__(self):
+
+
+    @classmethod
+    def __from_file(cls, filename):
         with open(filename, 'r') as stream:
             try:
-                self.__content = yaml.safe_load(stream)
-                print(self.__content)
+                self.__ymlContent = yaml.safe_load(stream)
 
             except yaml.YAMLError as exc:
                 print(exc)
 
     @classmethod
     def default(cls):
-        kiwi_root = os.environ.get('KIWI_ROOT')
+        result = cls.__from_file(cls.KIWI_ROOT + "/default.kiwi.yml")
 
-        cfg = cls(kiwi_root + "/default.kiwi.yml")
-        with open(kiwi_root + "/version-tag", 'r') as stream:
-            cfg.__content["version"] = stream.read().strip()
+        with open(cls.KIWI_ROOT + "/version-tag", 'r') as stream:
+            result.__ymlContent["version"] = stream.read().strip()
 
-        return cfg
+        return result
 
-    def __user_input(self, path, key, prompt):
-        content = self.__content
+    def __user_input(self, key, prompt):
+        """"""
 
+        # "a:b:c" => path = ['a', 'b'], key = 'c'
+        path = key.split(':')
+        (path, key) = (path[:-1], path[-1])
+
+        # resolve path
+        content = self.__ymlContent
         for step in path:
             content = content[step]
 
-        try:
-            result = input("{} [Default: {}] ".format(prompt, content[key])).strip()
-        except:
-            result = None
+        # prompt user as per argument
+        result = input("{} [Default: {}] ".format(prompt, content[key])).strip()
 
+        #
         if result:
             content[key] = result
 
-    def init(self):
-        self.__user_input([], "version", "Choose kiwi-config version")
+    def user_input(self):
+        self.__user_input("version", "Choose kiwi-config version")
 
-        self.__user_input(["suffix"], "project", "Enter suffix for project directories")
-        self.__user_input(["suffix"], "down", "Enter suffix for disabled projects")
+        self.__user_input("suffixes:project", "Enter suffix for project directories")
+        self.__user_input("suffixes:down", "Enter suffix for disabled projects")
 
-        self.__user_input(["docker"], "net", "Enter ")
-        self.__user_input(["docker"], "cidr", "Enter ")
+        self.__user_input("network:name", "Enter name for docker network")
+        self.__user_input("network:cidr", "Enter ")
 
-        self.__user_input(["target"], "root", "Enter ")
+        self.__user_input("storage:location", "Enter ")
