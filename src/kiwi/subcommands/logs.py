5c4898577714fcffe88a7375080143fb7bedfca9
ldericher <jmmiehe@gmx.de>
2020-08-10 16:36:05 +0200

    "logs" command with "-f" switch

--- src/kiwi/subcommands/logs.py
+++ src/kiwi/subcommands/logs.py
@@ -1,3 +1,5 @@
+from ..core import Parser
+
 from ._utils import SubCommand, DockerProgram
 
 
@@ -5,12 +7,22 @@ class LogsCommand(SubCommand):
     def __init__(self):
         super().__init__(
             'logs',
-            description="Show logs of a project"
+            description="Show logs of a project or service"
+        )
+
+        self.get_parser().add_argument(
+            '-f', '--follow',
+            action='store_true',
+            help="output appended data as log grows"
         )
 
     def run(self):
+        args = ['logs', '-t']
+        if Parser().get_args().follow:
+            args = [*args, '-f', '--tail=10']
+
         DockerProgram('docker-compose').run(
-            ['logs', '-tf', '--tail=10'],
+            args,
             cwd='hello-world.project',
             env={'COMPOSE_PROJECT_NAME': 'hello-world'}
         )
