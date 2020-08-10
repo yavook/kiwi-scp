3cb195fe1aaab29f9c42853d009bc2a9d69d0006
ldericher <jmmiehe@gmx.de>
2020-08-10 15:48:15 +0200

    Runner logic back into package, as singleton

---
+++ src/kiwi/runner.py
@@ -0,0 +1,40 @@
+from .subcommands import *
+
+###########
+# CONSTANTS
+
+SUBCOMMANDS = [
+    InitCommand,
+    ShowCommand,
+    LogsCommand
+]
+
+
+class Runner:
+    class __Runner:
+        __commands = []
+
+        def __init__(self):
+            for cmd in SUBCOMMANDS:
+                self.__commands.append(cmd())
+
+        def run(self, command_name):
+            for cmd in self.__commands:
+                if str(cmd) == command_name:
+                    cmd.run()
+                    return True
+
+            return False
+
+    __instance = None
+
+    def __init__(self):
+        if Runner.__instance is None:
+            Runner.__instance = Runner.__Runner()
+
+    def __getattr__(self, item):
+        return getattr(self.__instance, item)
+
+
+if __name__ == 'kiwi.runner':
+    Runner().setup_all()
--- src/kiwi/subcommands/logs.py
+++ src/kiwi/subcommands/logs.py
@@ -4,15 +4,11 @@ from ._utils import SubCommand, Docker
 
 
 class LogsCommand(SubCommand):
-    command = 'logs'
-
-    @classmethod
-    def setup(cls):
-        _ = Parser().get_subparsers().add_parser(
-            cls.command,
+    def __init__(self):
+        super().__init__(
+            'logs',
             description="Show logs of a project"
         )
 
-    @classmethod
-    def run(cls):
+    def run(self):
         print(Docker.run_command('docker-compose', ['logs', '-tf', '--tail=10'], cwd='hello-world.project', env={'COMPOSE_PROJECT_NAME': 'hello-world'}))
