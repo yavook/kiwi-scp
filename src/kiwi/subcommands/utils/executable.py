0a4e0d0e4c2f861b61f1b9991de011e7b537a5ef
ldericher <jmmiehe@gmx.de>
2020-08-12 17:18:20 +0200

    utils.executable based on PATH instead of YML

--- src/kiwi/subcommands/utils/executable.py
+++ src/kiwi/subcommands/utils/executable.py
@@ -1,8 +1,6 @@
 import os
-
-
-def get_exe_key(exe_name):
-    return f'executables:{exe_name}'
+import logging
+import subprocess
 
 
 def is_executable(filename):
@@ -18,4 +16,44 @@ def find_exe_file(exe_name):
         if is_executable(exe_file):
             return exe_file
 
-    return None
+    raise FileNotFoundError(f"Executable '{exe_name}' not found in $PATH!")
+
+
+class Executable:
+    __exe_name = None
+    __instances = {}
+
+    class __Executable:
+        __cmd = None
+
+        def __init__(self, exe_name, requires_root=False):
+            self.__cmd = [find_exe_file(exe_name)]
+
+            if requires_root:
+                self.__cmd = [find_exe_file("sudo"), *self.__cmd]
+
+        def __build_cmd(self, args, **kwargs):
+            cmd = [*self.__cmd, *args]
+            logging.debug(f"Executable cmd{cmd}, kwargs{kwargs}")
+            return cmd
+
+        def run(self, args, **kwargs):
+            return subprocess.run(
+                self.__build_cmd(args, **kwargs),
+                **kwargs
+            )
+
+        def Popen(self, args, **kwargs):
+            return subprocess.Popen(
+                self.__build_cmd(args, **kwargs),
+                **kwargs
+            )
+
+    def __init__(self, exe_name, requires_root=False):
+        self.__exe_name = exe_name
+
+        if exe_name not in Executable.__instances:
+            Executable.__instances[exe_name] = Executable.__Executable(exe_name, requires_root)
+
+    def __getattr__(self, item):
+        return getattr(self.__instances[self.__exe_name], item)
