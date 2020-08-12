70b04f039a637bb30a9b8b3d566e941372007687
ldericher <jmmiehe@gmx.de>
2020-08-12 17:39:55 +0200

    DockerCommand now is an Executable

--- src/kiwi/subcommands/utils/dockercommand.py
+++ src/kiwi/subcommands/utils/dockercommand.py
@@ -3,33 +3,39 @@ import subprocess
 from .executable import Executable
 
 
-class DockerCommand:
+class DockerCommand(Executable):
     __requires_root = None
-    __exe = None
 
     def __init__(self, exe_name):
+        super().__init__(exe_name)
+
         if DockerCommand.__requires_root is None:
             try:
                 Executable('docker').run(
-                    ['ps'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+                    ['ps'],
+                    check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
                 )
                 DockerCommand.__requires_root = False
             except subprocess.CalledProcessError:
                 DockerCommand.__requires_root = True
 
-        self.__exe = Executable(exe_name, DockerCommand.__requires_root)
-
-    def __getattr__(self, item):
-        return getattr(self.__exe, item)
+    def run(self, args, **kwargs):
+        # equivalent to 'super().run' but agnostic of nested class construct
+        super().__getattr__("run")(
+            args, DockerCommand.__requires_root,
+            **kwargs
+        )
 
     def run_less(self, args, **kwargs):
         process = self.Popen(
-            args, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
+            args, DockerCommand.__requires_root,
+            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
             **kwargs
         )
 
         less_process = Executable('less').run(
-            ['-R', '+G'], stdin=process.stdout
+            ['-R', '+G'],
+            stdin=process.stdout
         )
 
         process.communicate()
