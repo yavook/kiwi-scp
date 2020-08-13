ce4c121bb01de6f6ee6f4b8fc58c0ec319abceca
ldericher <jmmiehe@gmx.de>
2020-08-13 13:00:32 +0200

    Commands "cmd" and "sh"

--- src/kiwi/subcommands/__init__.py
+++ src/kiwi/subcommands/__init__.py
@@ -2,9 +2,13 @@
 from .init import InitCommand
 from .logs import LogsCommand
 from .show import ShowCommand
+from .cmd import CmdCommand
+from .shell import ShellCommand
 
 __all__ = [
     'InitCommand',
     'LogsCommand',
-    'ShowCommand'
+    'ShowCommand',
+    'CmdCommand',
+    'ShellCommand'
 ]
