fe094fbecb737928ac085efdd4b00599a6bfd6ef
ldericher <jmmiehe@gmx.de>
2020-08-17 12:08:42 +0200

    subcommand "net-up"

--- src/kiwi/subcommands/__init__.py
+++ src/kiwi/subcommands/__init__.py
@@ -2,6 +2,7 @@
 from .cmd import CmdCommand
 from .init import InitCommand
 from .logs import LogsCommand
+from .net_up import NetUpCommand
 from .sh import ShCommand
 from .show import ShowCommand
 
@@ -9,6 +10,7 @@ __all__ = [
     'CmdCommand',
     'InitCommand',
     'LogsCommand',
+    'NetUpCommand',
     'ShCommand',
     'ShowCommand',
 ]
