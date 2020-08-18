a3421e6501cd2cb14b31eb0d079c5425ceed92a2
ldericher <jmmiehe@gmx.de>
2020-08-18 13:50:21 +0200

    FlexCommand class

--- src/kiwi/subcommands/up.py
+++ src/kiwi/subcommands/up.py
@@ -2,38 +2,31 @@
 import logging
 
 # local
-from ._subcommand import ServiceCommand
+from ._subcommand import FlexCommand
 from .utils.dockercommand import DockerCommand
 from .utils.project import get_project_name, list_projects
 
 
-class UpCommand(ServiceCommand):
+class UpCommand(FlexCommand):
     """kiwi up"""
 
     def __init__(self):
         super().__init__(
-            'up', num_projects='?', num_services='*',
-            description="Bring up the whole instance, a project or service(s) inside a project"
+            'up', description="Bring up the whole instance, a project or service(s) inside a project"
         )
 
-    def run(self, runner, config, args):
-        if 'projects' not in args or args.projects is None:
-            # "up" for all projects
-            for project_name in list_projects(config):
-                args.projects = project_name
-                runner.run('up')
+    def _run_project(self, runner, config, args):
+        logging.info(f"Bringing up project '{get_project_name(args)}'")
 
-            return
+        DockerCommand('docker-compose').run(
+            config, args, ['up', '-d']
+        )
+        return True
 
-        if 'services' in args and args.services:
-            logging.info(f"Bringing up services {args.services} in project '{get_project_name(args)}'")
-        else:
-            logging.info(f"Bringing up project '{get_project_name(args)}'")
+    def _run_services(self, runner, config, args):
+        logging.info(f"Bringing up services {args.services} in project '{get_project_name(args)}'")
 
-        if runner.run('net-up'):
-            DockerCommand('docker-compose').run(
-                config, args, ['up', '-d', *args.services]
-            )
-            return True
-
-        return False
+        DockerCommand('docker-compose').run(
+            config, args, ['up', '-d', *args.services]
+        )
+        return True
