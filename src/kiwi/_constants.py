79ecca2322bad16844c19a4a73b9e55ddedecbea
ldericher <jmmiehe@gmx.de>
2020-08-18 12:34:09 +0200

    dir structure

--- src/kiwi/_constants.py
+++ src/kiwi/_constants.py
@@ -15,9 +15,9 @@ KIWI_CONF_NAME = os.getenv('KIWI_CONF_NAME', "kiwi.yml")
 # FILE NAMES
 
 # text files inside kiwi-config "src" directory
-HEADER_KIWI_CONF_NAME = f"{KIWI_ROOT}/kiwi_header.yml"
-DEFAULT_KIWI_CONF_NAME = f"{KIWI_ROOT}/kiwi_default.yml"
-VERSION_TAG_NAME = f"{KIWI_ROOT}/version-tag"
+HEADER_KIWI_CONF_NAME = f"{KIWI_ROOT}/etc/kiwi_header.yml"
+DEFAULT_KIWI_CONF_NAME = f"{KIWI_ROOT}/etc/kiwi_default.yml"
+VERSION_TAG_NAME = f"{KIWI_ROOT}/etc/version_tag"
 
 # special config directory in projects
 CONF_DIRECTORY_NAME = 'conf'
