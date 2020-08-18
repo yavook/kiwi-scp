81b597cb586d78dfd06f4ddb39704139f5ae745f
ldericher <jmmiehe@gmx.de>
2020-08-18 15:05:19 +0200

    neater prompts

--- src/kiwi/subcommands/update.py
+++ src/kiwi/subcommands/update.py
@@ -1,5 +1,6 @@
 # local
 from ._subcommand import FlexCommand
+from .utils.misc import are_you_sure
 
 
 class UpdateCommand(FlexCommand):
@@ -10,6 +11,18 @@ class UpdateCommand(FlexCommand):
             'update', "Updating",
             description="Update the whole instance, a project or service(s) inside a project"
         )
+
+    def _run_instance(self, runner, config, args):
+        if are_you_sure([
+            "This will update the entire instance at once.",
+            "",
+            "This is probably not what you intended, because:",
+            " - Updates may take a long time",
+            " - Updates may break beloved functionality",
+        ]):
+            return super()._run_instance(runner, config, args)
+
+        return False
 
     def _run_services(self, runner, config, args, services):
         result = runner.run('build')
