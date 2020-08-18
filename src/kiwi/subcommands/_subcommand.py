6615982e95137239c8826db40977bbf2f3d8503f
ldericher <jmmiehe@gmx.de>
2020-08-18 13:50:21 +0200

    FlexCommand class

--- src/kiwi/subcommands/_subcommand.py
+++ src/kiwi/subcommands/_subcommand.py
@@ -1,3 +1,6 @@
+# local
+from .utils.project import get_project_name, list_projects
+
 # parent
 from ..parser import Parser
 
@@ -63,3 +66,39 @@ class ServiceCommand(ProjectCommand):
             'services', metavar='service', nargs=num_services, type=str,
             help=f"select {services} in a project"
         )
+
+
+class FlexCommand(ServiceCommand):
+    def __init__(self, name, **kwargs):
+        super().__init__(
+            name, num_projects='?', num_services='*',
+            **kwargs
+        )
+
+    def _run_instance(self, runner, config, args):
+        result = True
+
+        for project_name in list_projects(config):
+            args.projects = project_name
+            result &= runner.run(str(self))
+
+        return result
+
+    def _run_project(self, runner, config, args):
+        pass
+
+    def _run_services(self, runner, config, args):
+        pass
+
+    def run(self, runner, config, args):
+        if 'projects' not in args or args.projects is None:
+            # command for entire instance
+            return self._run_instance(runner, config, args)
+
+        elif 'services' not in args or not args.services:
+            # command for whole project
+            return self._run_project(runner, config, args)
+
+        else:
+            # command for service(s) inside project
+            return self._run_services(runner, config, args)
--- src/kiwi/subcommands/down.py
+++ src/kiwi/subcommands/down.py
@@ -2,48 +2,41 @@
 import logging
 
 # local
-from ._subcommand import ServiceCommand
+from ._subcommand import FlexCommand
 from .utils.dockercommand import DockerCommand
 from .utils.project import get_project_name, list_projects
 from .utils.user_input import are_you_sure
 
 
-class DownCommand(ServiceCommand):
+class DownCommand(FlexCommand):
     """kiwi down"""
 
     def __init__(self):
         super().__init__(
-            'down', num_projects='?', num_services='*',
-            description="Bring down the whole instance, a project or service(s) inside a project"
+            'down', description="Bring down the whole instance, a project or service(s) inside a project"
         )
 
-    def run(self, runner, config, args):
-        if 'projects' not in args or args.projects is None:
-            # "down" for all projects
-            if are_you_sure("This will bring down the entire instance."):
-                for project_name in list_projects(config):
-                    args.projects = project_name
-                    runner.run('down')
-            else:
-                return False
+    def _run_instance(self, runner, config, args):
+        if are_you_sure("This will bring down the entire instance."):
+            super()._run_instance(runner, config, args)
 
-        elif 'services' in args and args.services:
-            # "down" for service(s) inside project
-            logging.info(f"Bringing down services {args.services} in project '{get_project_name(args)}'")
+        return False
 
-            DockerCommand('docker-compose').run(
-                config, args, ['stop', *args.services]
-            )
-            DockerCommand('docker-compose').run(
-                config, args, ['rm', '-f', *args.services]
-            )
+    def _run_project(self, runner, config, args):
+        logging.info(f"Bringing down project '{get_project_name(args)}'")
 
-        else:
-            # "down" for project
-            logging.info(f"Bringing down project '{get_project_name(args)}'")
+        DockerCommand('docker-compose').run(
+            config, args, ['down']
+        )
+        return True
 
-            DockerCommand('docker-compose').run(
-                config, args, ['down']
-            )
+    def _run_services(self, runner, config, args):
+        logging.info(f"Bringing down services {args.services} in project '{get_project_name(args)}'")
 
+        DockerCommand('docker-compose').run(
+            config, args, ['stop', *args.services]
+        )
+        DockerCommand('docker-compose').run(
+            config, args, ['rm', '-f', *args.services]
+        )
         return True
