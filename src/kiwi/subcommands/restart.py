fc2c9cb04ef80de02dc127527461692052f7a6e4
ldericher <jmmiehe@gmx.de>
2020-08-25 15:42:53 +0200

    Subcommand "restart"

---
+++ src/kiwi/subcommands/restart.py
@@ -0,0 +1,26 @@
+# local
+from ..subcommand import ServiceCommand
+from ..misc import are_you_sure
+
+
+class RestartCommand(ServiceCommand):
+    """kiwi restart"""
+
+    def __init__(self):
+        super().__init__(
+            'restart', num_projects='?', num_services='*',
+            action="Restarting",
+            description="Restart the whole instance, a project or service(s) inside a project"
+        )
+
+    def _run_instance(self, runner, args):
+        if are_you_sure([
+            "This will restart the entire instance."
+        ]):
+            return super()._run_instance(runner, args)
+
+        return False
+
+    def _run_services(self, runner, args, project, services):
+        project.compose_run(['restart', *services])
+        return True
