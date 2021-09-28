4f29c8107ed8d0efae29a8b42531fa1d792bae09
ldericher <40151420+ldericher@users.noreply.github.com>
2021-09-28 13:23:15 +0200

    Pipenv => Poetry

--- setup.py
+++ setup.py
@@ -3,21 +3,29 @@ from setuptools import setup, find_packages
 setup(
     name="kiwi_scp",
     version="0.1.6",
+    description="kiwi is the simple tool for managing container servers.",
+    long_description=open("README.md").read(),
     packages=find_packages(),
+    license="LICENSE",
+
     author="LDericher",
     author_email="ldericher@gmx.de",
+
     setup_requires="setuptools-pipfile",
     use_pipfile=True,
+
     entry_points={
         "console_scripts": [
             "kiwi = kiwi_scp.scripts.kiwi:main"
         ],
     },
     data_files=[
-        ("", ["data/etc/kiwi_help.txt"])
+        ('./kiwi_scp/', [
+            "*.txt",
+            "*.yml",
+            "*.Dockerfile",
+            "data/etc/version_tag",
+        ])
     ],
     include_package_data=True,
-    license="LICENSE",
-    description="kiwi is the simple tool for managing container servers.",
-    long_description=open("README.md").read(),
 )
