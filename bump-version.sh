f66466de949831f5be92880b42f66c1a6505b509
ldericher <40151420+ldericher@users.noreply.github.com>
2021-09-30 17:52:23 +0200

    bump version

--- bump-version.sh
+++ bump-version.sh
@@ -6,7 +6,6 @@ this_dir="$(dirname "${this}")"
 git_branch="$(git rev-parse --abbrev-ref HEAD)"
 git_tag="$(git describe --abbrev=0)"
 version_str="${git_branch##*/}"
-version_str="0.1.6"
 
 echo "${version_str}" > "${this_dir}/kiwi_scp/data/etc/version_tag"
 sed -ri "s/(version\s*:).*$/\1 '${version_str}'/" "${this_dir}/example/kiwi.yml"
