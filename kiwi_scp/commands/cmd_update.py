cce894b9429e6c871b92c03dd585404d7ba5117c
Jörn-Michael Miehe <miehe@dp-electronics.de>
2022-01-19 15:36:41 +0100

    update command import pattern

--- kiwi_scp/commands/cmd_update.py
+++ kiwi_scp/commands/cmd_update.py
@@ -4,6 +4,10 @@ import click
 from click import get_current_context
 
 from .cmd import KiwiCommandType, KiwiCommand
+from .cmd_build import BuildCommand
+from .cmd_down import DownCommand
+from .cmd_pull import PullCommand
+from .cmd_up import UpCommand
 from .decorators import kiwi_command
 from ..instance import Instance
 from ..project import Project
@@ -25,6 +29,20 @@ class UpdateCommand(KiwiCommand):
     enabled_only = True
 
     @classmethod
+    def run_for_instance(cls, instance: Instance, force: bool = None) -> None:
+        if not force:
+            if not KiwiCommand.danger_confirm(
+                    "This will update the entire instance at once.",
+                    "",
+                    "This may not be what you intended, because:",
+                    " - Updates may take a long time",
+                    " - Updates may break beloved functionality",
+            ):
+                return
+
+        super().run_for_instance(instance)
+
+    @classmethod
     def run_for_filtered_services(cls, instance: Instance, project: Project, services: Services,
                                   new_service_names: List[str], **kwargs) -> None:
         if not services:
@@ -35,14 +53,13 @@ class UpdateCommand(KiwiCommand):
             ):
                 return
 
-        from .cmd_build import BuildCommand
-        from .cmd_down import DownCommand
-        from .cmd_pull import PullCommand
-        from .cmd_up import UpCommand
-
         ctx = get_current_context()
+        assert isinstance(BuildCommand, click.Command)
         ctx.forward(BuildCommand)
+        assert isinstance(PullCommand, click.Command)
         ctx.forward(PullCommand)
         # TODO conf-copy
+        assert isinstance(DownCommand, click.Command)
         ctx.forward(DownCommand)
+        assert isinstance(UpCommand, click.Command)
         ctx.forward(UpCommand)
