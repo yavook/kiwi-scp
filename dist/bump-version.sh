2b0ec1cea214a4f2367b0b2aa46a544a977d00cf
ldericher <40151420+ldericher@users.noreply.github.com>
2022-02-21 16:44:22 +0100

    version_str based on tag

--- dist/bump-version.sh
+++ dist/bump-version.sh
@@ -3,9 +3,9 @@
 this="$(readlink -f "${0}")"
 this_dir="$(dirname "${this}")"
 
-git_branch="$(git rev-parse --abbrev-ref HEAD)"
+# git_branch="$(git rev-parse --abbrev-ref HEAD)"
 git_tag="$(git describe --abbrev=0)"
-version_str="${git_branch##*/}"
+version_str="${git_tag##*/}"
 version_str="0.2.0"
 
 sed -ri "s/(version\s*:).*$/\1 '${version_str}'/" "${this_dir}/../example/kiwi.yml"
